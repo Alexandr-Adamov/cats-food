71191fdd30648295ebdceb456ad56794ab1e9ef6
Alexandr Adamov <wofff_4@mail.ru>
2020-09-01 16:38:30 +0300

    Updates

---
+++ precache-manifest.bc53b9c1a29108bd0e9120280a8ee7dc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50e5b8c2f9a67a997c0f4b2d01e8f12",
+    "url": "/cats-food/index.html"
+  },
+  {
+    "revision": "cbb31ea7fe114473e1fc",
+    "url": "/cats-food/static/css/main.1b38fef1.chunk.css"
+  },
+  {
+    "revision": "8e8e691d3daba4295f2f",
+    "url": "/cats-food/static/js/2.05dee26e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cats-food/static/js/2.05dee26e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb31ea7fe114473e1fc",
+    "url": "/cats-food/static/js/main.49855743.chunk.js"
+  },
+  {
+    "revision": "7dcccc5b9c9ace7caf3f",
+    "url": "/cats-food/static/js/runtime-main.dc5dc408.js"
+  },
+  {
+    "revision": "79d68011797f7124dd17f3a480a7a01f",
+    "url": "/cats-food/static/media/941afad4465c7704abe8996ef0f495742e3963a5.79d68011.png"
+  },
+  {
+    "revision": "d727aee2fcb615a5e85517793448fe64",
+    "url": "/cats-food/static/media/blue.d727aee2.png"
+  },
+  {
+    "revision": "0523619046b371e7e27e7e9fc7594d92",
+    "url": "/cats-food/static/media/grey.05236190.png"
+  },
+  {
+    "revision": "20195368f273a59ec7676d7184800b57",
+    "url": "/cats-food/static/media/red.20195368.png"
+  }
+]);
